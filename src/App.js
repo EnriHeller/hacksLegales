250fc86a9385ca400d05286a69a431f7912bf095
Enrique Heller <enrique.heller@hotmail.com>
2023-12-21 16:59:09 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "style.css"
+import "tailwindcss/lib/css/preflight.css"
+import EventLandingPage from "pages/EventLandingPage"
+
+function App() {
+  return (
+    <EventLandingPage/>
+  )
+}
+
+export default App
